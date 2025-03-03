89155acb8a6e1c403deb2ec9c930d2ca2163f2e2
Eric Winebrenner <ewinebrenner@champlain.edu>
2025-03-03 09:44:26 -0500

    Using DoTween for movement

--- GridGame01/Assets/GridMovement.cs
+++ GridGame01/Assets/GridMovement.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,29 +7,47 @@ public class GridMovement : MonoBehaviour
 {
     [SerializeField] private GridManager _gridManager;
     [SerializeField] private Vector2Int _gridPos;
+    [SerializeField] private float _moveDuration = 0.5f;
+    //Built in Ease function
+    [SerializeField] private Ease _moveEase = Ease.InOutCubic;
+    //Custom ease function
+    [SerializeField] private AnimationCurve _moveCurve;
+    Tween _moveTween;
+    void GoToPosition(Vector2Int pos)
+    {
+        if (_moveTween != null && _moveTween.IsActive())
+            return;
 
+        _gridPos = pos;
+        GameObject tile = _gridManager.GetTile(_gridPos.x, _gridPos.y);
+        Vector3 targetPos = tile.transform.position;
+        Sequence seq = DOTween.Sequence();
+        seq.Append(transform.DOMove(targetPos, _moveDuration).SetEase(_moveCurve));
+      //  seq.Append(transform.DOPunchScale(Vector3.one * 1.5f, 0.3f));
+        _moveTween = seq;
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.D))
         {
-            _gridPos.x++;
+            GoToPosition(_gridPos + new Vector2Int(1,0));
         }
         if (Input.GetKeyDown(KeyCode.A))
         {
-            _gridPos.x--;
+            GoToPosition(_gridPos + new Vector2Int(-1, 0));
         }
         if (Input.GetKeyDown(KeyCode.W))
         {
-            _gridPos.y++;
+            GoToPosition(_gridPos + new Vector2Int(0, 1));
         }
         if (Input.GetKeyDown(KeyCode.S))
         {
-            _gridPos.y--;
+            GoToPosition(_gridPos + new Vector2Int(0, -1));
         }
 
         //Look up tile at grid position
-        GameObject tile = _gridManager.GetTile(_gridPos.x, _gridPos.y);
+
         //Move worldspace position to match current tile
-        transform.position = Vector3.MoveTowards(transform.position, tile.transform.position, 5.0f * Time.deltaTime);
+        //  transform.position = Vector3.MoveTowards(transform.position, tile.transform.position, 5.0f * Time.deltaTime);
     }
 }
